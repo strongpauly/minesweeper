4b4588c73aaa9e5eab2c15013e5eb6987c274d32
Paul Potsides <strongpauly@hotmail.com>
2017-06-16 15:56:14 +0100

    Add enzyme and sinon to allow proper testing with and without DOM.

--- src/Cell.test.js
+++ src/Cell.test.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import Cell from './Cell';
-import renderer from 'react-test-renderer';
+import {mount, shallow} from 'enzyme';
+import sinon from 'sinon';
 
 /* eslint-env jest */
 
 describe('Cell', () => {
 
-  function render(){
-    return renderer.create(<Cell x={0} y={0}/>);
-  }
-
   it('renders without crashing', () => {
-    render();
+    shallow(<Cell x={0} y={0}/>);
   });
 
-  it('has not changed', () => {
-    let cell = render().toJSON();
+  it('has "cell" css class', () => {
+    const cell = shallow(<Cell x={0} y={0}/>);
     expect(cell).toMatchSnapshot();
+    expect(cell.find('.cell')).toHaveLength(1);
+  });
+
+  it('initialised with checked has "checked" class', () => {
+    const cell = shallow(<Cell x={0} y={0} checked={true}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('.checked')).toHaveLength(1);
+  });
+
+  it('initialised with mine has "mine" class', () => {
+    const cell = shallow(<Cell x={0} y={0} mine={true}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('.mine')).toHaveLength(1);
+  });
+
+  it('initialised with mine shows mine img', () => {
+    const cell = shallow(<Cell x={0} y={0} mine={true}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('img')).toHaveLength(1);
+  });
+
+  it('initialised with marked has "marked" class', () => {
+    const cell = shallow(<Cell x={0} y={0} marked={true}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('.marked')).toHaveLength(1);
+  });
+
+  it('click fires onCheck handler', () => {
+    const onCheck = sinon.spy();
+    let table = mount(<table><tbody><tr><Cell x={0} y={0} onCheck={onCheck}/></tr></tbody></table>);
+    table.find('td').simulate('click');
+    expect(onCheck.calledOnce).toEqual(true);
+  });
+
+  it('context menu fires onMarked handler', () => {
+    const onMark = sinon.spy();
+    let table = mount(<table><tbody><tr><Cell x={0} y={0} onMark={onMark}/></tr></tbody></table>);
+    table.find('td').simulate('contextMenu');
+    expect(onMark.calledOnce).toEqual(true);
   });
 });
