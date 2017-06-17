66ea140987636c7769cebdac8fb2052fc3725775
Paul Potsides <strongpauly@hotmail.com>
2017-06-17 10:28:55 +0100

    100% Cell coverage.

--- src/Cell.test.js
+++ src/Cell.test.js
@@ -41,6 +41,19 @@ describe('<Cell>', () => {
     expect(cell.find('.marked')).toHaveLength(1);
   });
 
+  it('with a marked mine show an X', () => {
+    const cell = shallow(<Cell x={0} y={0} mine={true} marked={true}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('td').text()).toEqual('X');
+  });
+
+  it('initialised with an adjacent count, renders that count with "count-" class', () => {
+    const cell = shallow(<Cell x={0} y={0} checked={true} adjacentCount={1}/>);
+    expect(cell).toMatchSnapshot();
+    expect(cell.find('.count-1')).toHaveLength(1);
+    expect(cell.find('td').text()).toEqual('1');
+  });
+
   it('click fires onCheck handler', () => {
     const onCheck = sinon.spy();
     let table = mount(<table><tbody><tr><Cell x={0} y={0} onCheck={onCheck}/></tr></tbody></table>);
@@ -53,5 +66,28 @@ describe('<Cell>', () => {
     let table = mount(<table><tbody><tr><Cell x={0} y={0} onMark={onMark}/></tr></tbody></table>);
     table.find('td').simulate('contextMenu');
     expect(onMark.calledOnce).toEqual(true);
+  });
+
+  it('click won\'t fire onCheck if checked is true', () => {
+    const onCheck = sinon.spy();
+    let table = mount(<table><tbody><tr><Cell x={0} y={0} onCheck={onCheck} checked={true}/></tr></tbody></table>);
+    let td = table.find('td');
+    td.simulate('click');
+    expect(onCheck.calledOnce).toEqual(false);
+  });
+
+  it('click won\'t fire onCheck if marked is true', () => {
+    const onCheck = sinon.spy();
+    let table = mount(<table><tbody><tr><Cell x={0} y={0} onCheck={onCheck} marked={true}/></tr></tbody></table>);
+    let td = table.find('td');
+    td.simulate('click');
+    expect(onCheck.calledOnce).toEqual(false);
+  });
+
+  it('click won\'t fire onMark if checked is true', () => {
+     const onMark = sinon.spy();
+    let table = mount(<table><tbody><tr><Cell x={0} y={0} onMark={onMark} checked={true}/></tr></tbody></table>);
+    table.find('td').simulate('contextMenu');
+    expect(onMark.calledOnce).toEqual(false);
   });
 });
